625782e17bb9f1bf3b554621f1ff17b70a7c4ecb
Ripudaman Singh <rds0751@gmail.com>
2020-10-21 13:12:31 +0530

    first commit

--- djadmin/settings.py
+++ djadmin/settings.py
@@ -4,21 +4,21 @@ from __future__ import unicode_literals
 
 from django.conf import settings
 
-DJADMIN_DYNAMIC_FIELD_DISPLAY = getattr(settings, 'DJADMIN_DYNAMIC_FIELD_DISPLAY', False)
+DJADMIN_DYNAMIC_FIELD_DISPLAY = getattr(settings, 'DJADMIN_DYNAMIC_FIELD_DISPLAY', True)
 
-ADMIN_COLOR_THEME = getattr(settings, 'ADMIN_COLOR_THEME', 'cyan')
+ADMIN_COLOR_THEME = getattr(settings, 'ADMIN_COLOR_THEME', 'blue')
 
-ALLOW_FORGET_PASSWORD_ADMIN = getattr(settings, 'ALLOW_FORGET_PASSWORD_ADMIN', False)
+ALLOW_FORGET_PASSWORD_ADMIN = getattr(settings, 'ALLOW_FORGET_PASSWORD_ADMIN', True)
 
-ADMIN_HEADER_TITLE = getattr(settings, 'ADMIN_HEADER_TITLE', 'Django administrator')
+ADMIN_HEADER_TITLE = getattr(settings, 'ADMIN_HEADER_TITLE', 'JR Admin')
 
 DJADMIN_DYNAMIC_DELETE_UNREGISTER_FIELD = getattr(settings, 'DJADMIN_DYNAMIC_DELETE_UNREGISTER_FIELD', False)
 
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'users.User')
 
 DJADMIN_FIELD_DEPTH = getattr(settings, 'DJADMIN_FIELD_DEPTH', 1)
 
-DJADMIN_CONFIG_PAGE = getattr(settings, 'DJADMIN_CONFIG_PAGE', False)
+DJADMIN_CONFIG_PAGE = getattr(settings, 'DJADMIN_CONFIG_PAGE', True)
 
 DJADMIN_MANAGE_FILE_NAME = getattr(settings, 'DJADMIN_MANAGE_FILE_NAME', 'manage.py')
 
